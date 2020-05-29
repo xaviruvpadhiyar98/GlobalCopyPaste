cbfcda4ba2db14159aa7830ecb6a264c74a46b57
Dhruv Padhiyar <dhruvpadhiyar98@gmail.com>
2020-05-29 12:13:57 +0530

    Removed Jsonify

--- copyPaste.py
+++ copyPaste.py
@@ -1,19 +1,17 @@
 from pyperclip import paste,copy
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from json import loads
 
 
 app = Flask(__name__)
-
 @app.route("/",methods=["POST","GET"])
 def CopyPaste():
 	if request.method == 'POST':
 		clipboardText = copy(loads(request.data.decode("utf-8"))["1"])
-		return jsonify(clipboardText)
+		return str(clipboardText)
 	if request.method == 'GET':
 		clipboardText = paste()
-		return jsonify(clipboardText)
+		return clipboardText
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0",debug=True)
-
